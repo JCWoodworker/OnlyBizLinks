d6e2db13e2f798767e61f7d510c002cd95f19e44
JCWoodworker <jc@rilocalwoodworks.com>
2024-10-13 18:01:25 -0400

    styled custom links with box shadow and hover effects

--- src/custom-components/LinkWrapper.tsx
+++ src/custom-components/LinkWrapper.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@mui/material"
+import { Link, Box } from "@mui/material"
 
 type LinkProps = {
 	url: string
@@ -7,9 +7,35 @@ type LinkProps = {
 
 const LinkWrapper: React.FC<LinkProps> = ({ url, title }) => {
 	return (
-		<Link href={url} target="_blank" sx={{ textDecoration: "none" }}>
-			{title}
-		</Link>
+		<Box
+			sx={{
+				display: 'flex',
+				width: '100%',
+			}}
+		>
+			<Link
+				href={url}
+				target="_blank"
+				sx={{
+					textDecoration: "none",
+					color: "inherit",
+					padding: "4px 8px",
+					borderRadius: "4px",
+					border: "1px solid #ccc",
+					boxShadow: "0 4px 6px rgba(0, 0, 0, 0.15)",
+					transition: "all 0.5s ease",
+					flex: 1,
+					display: 'block',
+					textAlign: 'center',
+					"&:hover": {
+						fontWeight: "bold",
+						boxShadow: "0 6px 12px rgba(0, 0, 0, 0.55)",
+					},
+				}}
+			>
+				{title}
+			</Link>
+		</Box>
 	)
 }
 
