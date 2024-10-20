9e7bf2465e215d025b0c46dd24478c49538f7801
JCWoodworker <jc@rilocalwoodworks.com>
2024-10-19 23:05:40 -0400

    minor changes to home page and added new packages

--- src/web-app/public-home/PublicHome.tsx
+++ src/web-app/public-home/PublicHome.tsx
@@ -8,8 +8,11 @@ const PublicHome = () => {
 	]
 	return (
 		<Box sx={{ textAlign: "center" }}>
-			<Typography variant="h2">
-				OnlyBizLinks.com
+			<Typography variant="h2">OnlyBizLinks</Typography>
+			<Divider sx={{ my: 2 }} />
+			<Typography variant="body1">
+				OnlyBizLinks is a service that allows you to create a list custom links
+				for your business.
 			</Typography>
 			<Divider sx={{ my: 2 }} />
 			<Typography variant="body1">Current Subscribers:</Typography>
